3da77bb50ccf720a23fc8e2b29798ae1041182ec
shguanWHU <shaohengguan@gmail.com>
2025-01-16 13:11:40 +0000

    without python

--- install.cmake.bat
+++ install.cmake.bat
@@ -95,7 +95,7 @@ if exist build_cmake (
     echo "Removed existing build directory."
 )
 
-cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_USE_WAYLAND=OFF -DGLFW_BUILD_X11=OFF  -DWITH_PYTHON=ON -DCMAKE_INSTALL_PREFIX=PretextViewAI.windows -DCMAKE_PREFIX_PATH=%TORCH_PATH% -S . -B build_cmake
+cmake -DCMAKE_BUILD_TYPE=Release -DGLFW_USE_WAYLAND=OFF -DGLFW_BUILD_X11=OFF -DCMAKE_INSTALL_PREFIX=PretextViewAI.windows -DCMAKE_PREFIX_PATH=%TORCH_PATH% -S . -B build_cmake
 if errorlevel 1 (
     echo "CMake configuration failed."
     goto :error
